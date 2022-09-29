151538c3e1705e79474d10cb1d7049bfd1de6e95
Ismael_Mendi <109171817+IsmaelMendi@users.noreply.github.com>
2022-09-29 17:50:02 -0500

    informacion en JavaScript - OpenBootcamp
    Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
    
    - Tu nombre (string)
    
    - Tu edad (number)
    
    - ¿Eres desarrollador? (boolean)
    
    - Tu fecha de nacimiento (Date)
    
    - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url

---
+++ Deber.js
@@ -0,0 +1,9 @@
+const date = new Date("november 05 2001");
+const libro = {
+    titulo:"Habitos atomicos",
+    autor: "James Clear",
+    fecha: 2018,
+    url: false,
+}
+const lista = ["ismael",20, false, date, libro];
+console.log(lista)   
