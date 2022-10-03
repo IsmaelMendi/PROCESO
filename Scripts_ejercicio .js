a0746e1e4969b1febd5a8c921950db639e963618
Ismael_Mendi <109171817+IsmaelMendi@users.noreply.github.com>
2022-10-02 23:54:45 -0500

    Ejercicio_scripts
    Crea un archivo JS que contenga las siguientes líneas
    
    - Una cadena de texto con tu Nombre
    
    - Otra cadena de texto con tu Apellido
    
    - Una cadena de texto que se llame "estudiante" concatenando tu Nombre y tu Apellido con un espacio entre medias
    
    - Una cadena de texto que se llame "estudianteMayus" que contenga la cadena estudiante pero todo en mayúsculas
    
    - Una cadena de texto que se llame "estudianteMinus" que contenga la cadena estudiante pero todo en minúsculas
    
    - Una variable que contenga el número de letras de la cadena "estudiante" contando los espacios
    
    - Una variable que contenga la primera letra del Nombre
    
    - Otra variable que contenga la última letra del Apellido
    
    - Una cadena de texto que elimine los espacios de la variable "estudiante"
    
    - Una variable booleana que diga si el Nombre está contenido en la variable "estudiante"

---
+++ Scripts_ejercicio .js
@@ -0,0 +1,26 @@
+let nombre= "Ismael ";
+let apellido= "Guanoluisa";
+let estudiante= (nombre.concat(apellido))
+
+console.log(estudiante);
+
+let estudianteMayus= "estudiante"
+console.log(estudianteMayus.toUpperCase());
+
+let estudianteMinus= "estudiante"
+console.log(estudianteMinus.toLocaleLowerCase())
+
+let total_letras= (estudiante.length)
+console.log(total_letras);
+
+let primera_name= (nombre[0])
+console.log(primera_name)
+
+let ultima_nickname= (apellido[9])
+console.log(ultima_nickname)
+
+let eliminar_espacios=(estudiante.trim().length)
+console.log(eliminar_espacios)
+
+let boolena= (estudiante.includes(nombre))
+console.log(boolena)
