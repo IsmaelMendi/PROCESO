f29a2ea81d9e8ab5ebc884bca1cd45f75cee7517
Ismael_Mendi <109171817+IsmaelMendi@users.noreply.github.com>
2022-10-03 21:11:25 -0500

    Numeros
    - Una variable que contenga tu altura en centímetros (entero)
    
    - Una variable que contenga tu altura en metros (número de coma flotante)
    
    - Una variable que contenga tu peso en kilogramos (número de coma flotante)
    
    - Una variable que contenga tu altura en metros redondeada hacia arriba
    
    - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
    
    - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript

---
+++ ejercicio de numeros.js
@@ -0,0 +1,16 @@
+let altura_cm= 185;
+
+let altura_mtr= 1.85;
+
+let peso_kg= 63.54;
+
+let peso2= peso_kg.toFixed()
+console.log(peso2)
+
+let peso3= peso_kg.toPrecision(7)
+console.log(peso3)
+
+let jsv= "el maximo valor que se puede obtener en Javascript + 1";
+let maximo= Number.MAX_VALUE;
+let resultado= (jsv + " = " + maximo);
+console.log(resultado)
